ff320fe76997786bdccd2bada1da0bf8a2b03575
Aleksei Izmalkin <aai95@yandex.ru>
2023-04-28 13:48:15 +0400

    Refactor project files

--- Tracker/ViewControllers/ViewController.swift
+++ Tracker/ViewControllers/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Алексей on 23.04.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
