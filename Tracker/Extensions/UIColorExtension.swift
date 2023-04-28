df7530b98047c09d493491d79c3ab30cdd73d6ee
Aleksei Izmalkin <aai95@yandex.ru>
2023-04-28 15:41:49 +0400

    Add base colors and make launch screen layout

---
+++ Tracker/Extensions/UIColorExtension.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UIColor {
+    static let ypBackgroundDay = UIColor(named: "YPBackgroundDay")!
+    static let ypBlackDay = UIColor(named: "YPBlackDay")!
+    static let ypWhiteDay = UIColor(named: "YPWhiteDay")!
+    
+    static let ypBackgroundNight = UIColor(named: "YPBackgroundNight")!
+    static let ypBlackNight = UIColor(named: "YPBlackNight")!
+    static let ypWhiteNight = UIColor(named: "YPWhiteNight")!
+    
+    static let ypGray = UIColor(named: "YPGray")!
+    static let ypLightGray = UIColor(named: "YPLightGray")!
+    static let ypRed = UIColor(named: "YPRed")!
+    static let ypBlue = UIColor(named: "YPBlue")!
+}
